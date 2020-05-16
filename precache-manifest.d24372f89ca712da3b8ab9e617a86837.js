ef55a19ccbe557d966d82a4fbf7e62c56ca07efd
hugomendonca98 <hugomendonca9@gmil.com>
2020-05-16 20:32:07 -0300

    Updates

---
+++ precache-manifest.d24372f89ca712da3b8ab9e617a86837.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f86fcbddf9c02db5292455ef9d1b664",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16b43d7575b05cde98b5",
+    "url": "/static/js/2.befa4719.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.befa4719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5155eb3fcd9f22e3b5d3",
+    "url": "/static/js/main.f1b674ae.chunk.js"
+  },
+  {
+    "revision": "56ac7d098bb0599506a1",
+    "url": "/static/js/runtime-main.733f9ba5.js"
+  },
+  {
+    "revision": "dc8b886b4cf2a2426a6664ad17ebf7f9",
+    "url": "/static/media/bg-boost-desktop.dc8b886b.svg"
+  },
+  {
+    "revision": "a82de60e4b42d5b6b6abf66a03a2dca1",
+    "url": "/static/media/bg-shorten-desktop.a82de60e.svg"
+  },
+  {
+    "revision": "1922b68a4d1dce29b2c40904cdbc1bba",
+    "url": "/static/media/icon-brand-recognition.1922b68a.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/static/media/icon-detailed-records.e3d9caa9.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/static/media/icon-fully-customizable.cc099cea.svg"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/static/media/illustration-working.5029d049.svg"
+  },
+  {
+    "revision": "6387840f9b6e8720165db8c73185bc51",
+    "url": "/static/media/logo.6387840f.svg"
+  }
+]);
